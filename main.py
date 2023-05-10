919e47f8bb03314237dc6db976da4ffca0525157
Amyr Francisco <amyrfrancisco11@gmail.com>
2023-05-10 18:39:38 +0800

    request API key

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import uvicorn
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, Header, HTTPException
 from chatplm.helpers.load_data import load_data
 from chatplm.model import ChatPLM
 
@@ -15,17 +15,46 @@ data = load_data()
 model = ChatPLM(data)
 
 
+API_KEY = os.environ.get("API_KEY")
+
+
 @app.get("/")
 def read_root():
     return {"msg": "This is ChatPLM API, go to /docs for more info."}
 
 
-@app.get("/chat/{prompt}")
-def chat(prompt: str):
+@app.get("/chat/")
+def chat(prompt: str, api_key: str = Header(None)):
+    authorize(api_key)
     response, confidence = model.response_from_model(prompt)
     return {"output": response, "confidence": float(confidence)}
 
 
+@app.post("/chat/feedback")
+async def submit_feedback(
+    input_message: str,
+    bot_response: str,
+    confidence_level: float,
+    api_key: str = Header(None)
+):
+    authorize(api_key)
+
+    save_feedback_to_database(
+        input_message, bot_response, confidence_level)
+
+    return {"input_message": input_message, "bot_response": bot_response, "confidence_level": confidence_level}
+
+
 @app.get("/metadata")
-def metadata():
+def metadata(api_key: str = Header(None)):
+    authorize(api_key)
     return {"trn_updated": data['date']}
+
+
+def authorize(api_key):
+    if api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Invalid API key")
+
+
+def save_feedback_to_database(input_message, bot_response, confidence_level):
+    pass
